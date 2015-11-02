3934135d00d9e2bdcbd7a769863e93d8202bc937
Deltharis <michal.torba@gmail.com>
2015-11-02 22:21:23 +0100

    DataSource from shiro.ini - casting doesn't work? dunno.

--- src/main/java/stja/shiro_stuff/MyCustomRealm.java
+++ src/main/java/stja/shiro_stuff/MyCustomRealm.java
@@ -2,14 +2,12 @@ package stja.shiro_stuff;
 
 import org.apache.shiro.authc.*;
 import org.apache.shiro.authc.credential.HashedCredentialsMatcher;
-import org.apache.shiro.jndi.JndiObjectFactory;
 import org.apache.shiro.realm.jdbc.JdbcRealm;
 import stja.data_access.UserDAO;
 import stja.entities.user.User;
 
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
-import javax.sql.DataSource;
 
 
 /**
@@ -35,10 +33,6 @@ public class MyCustomRealm extends JdbcRealm {
         setPermissionsLookupEnabled(true);
         setUserRolesQuery("select r.rolename from role r inner join UserRole ur on r.id = ur.roleId inner join User u on ur.userId = u.id where u.username = ?");
         setPermissionsQuery("select p.permission from Permission p inner join RolePermission rp on p.id = rp.permissionId inner join Role r on rp.roleId = r.id where r.roleName = ?");
-
-        JndiObjectFactory factory = new JndiObjectFactory();
-        factory.setResourceName("java:jboss/datasources/MySQLDS");
-        setDataSource((DataSource) factory);
     }
 
     @Override
