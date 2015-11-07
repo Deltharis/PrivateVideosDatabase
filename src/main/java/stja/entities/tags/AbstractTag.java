2622cf6fa9b9f4248c7c3d1d44e636e302d97755
Deltharis <michal.torba@gmail.com>
2015-11-07 03:55:34 +0100

    Equals and hashcode - now Tag and Person edit will work.

--- src/main/java/stja/entities/tags/AbstractTag.java
+++ src/main/java/stja/entities/tags/AbstractTag.java
@@ -15,10 +15,10 @@ public abstract class AbstractTag implements Serializable {
     @Id
     @GeneratedValue
     @Column(name = "id", nullable = false)
-    private int id;
+    protected int id;
 
     @Column(name = "text", length = 25)
-    private String text = "";
+    protected String text = "";
 
     public int getId() {
         return id;
@@ -39,5 +39,21 @@ public abstract class AbstractTag implements Serializable {
     @Override
     public String toString(){
         return getText();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        AbstractTag that = (AbstractTag) o;
+
+        return id == that.id;
+
+    }
+
+    @Override
+    public int hashCode() {
+        return id;
     }
 }
