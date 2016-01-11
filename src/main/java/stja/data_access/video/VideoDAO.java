f632fd64fb2f011a83159b5ff457a9dfedf7321b
Deltharis <michal.torba@gmail.com>
2016-01-11 20:51:29 +0100

    Default sort of videos by title. New favicon. Updated libraries.

--- src/main/java/stja/data_access/video/VideoDAO.java
+++ src/main/java/stja/data_access/video/VideoDAO.java
@@ -4,6 +4,7 @@ import stja.data_access.AbstractDAO;
 import stja.entities.video.Video;
 
 import javax.ejb.Stateless;
+import java.util.List;
 
 /**
  * Created by Delth on 17.10.2015.
@@ -18,5 +19,10 @@ public class VideoDAO extends AbstractDAO<Video> {
 
     public void testUpdate() {
         em.createNativeQuery("UPDATE Videos set core = 0").executeUpdate();
+    }
+
+    @Override
+    public List<Video> getAll() {
+        return em.createQuery("FROM Video ORDER BY title", Video.class).getResultList();
     }
 }
